cde21c8596e35ecc9c9701f9a5aaefe60707d6ae
janavipvel <janavipvel@gmail.com>
2026-02-26 15:34:28 +0530

    uc3- reverse palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,24 +1,26 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        Scanner sc = new Scanner(System.in);
 
-        String word = "madam1";
-        boolean isPalindrome = true;
-        int n = word.length() - 1;
-        for (int i = 0; i < word.length()/2; i++) {
-            if (word.charAt(i) != word.charAt(n - i)) {
-                isPalindrome = false;
-                break;
-            }
+        System.out.println("Palindrome Checker App");
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
 
+        String reversed = "";
+
+        // Step 1: Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
-        if (isPalindrome) {
-            System.out.println(word + " is a Palindrome");
+
+        // Step 2: Compare original and reversed using equals()
+        if (original.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println(word + " is not a Palindrome");
+            System.out.println("The given string is NOT a Palindrome.");
         }
+
+        sc.close();
 
     }
 }
